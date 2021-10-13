ad22dd204c97027521ec0a5e2b4769710ae0ee4c
Timo <38291523+lovetodream@users.noreply.github.com>
2021-10-13 17:01:49 +0200

    🔒️ Add id validation

--- src/app/dto/createAppDto.ts
+++ src/app/dto/createAppDto.ts
@@ -1,8 +1,10 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsMongoId } from 'class-validator';
 import { AppType } from '../../schemas/app.schema';
 
 @ArgsType()
 export class CreateAppDto {
+  @IsMongoId()
   @Field()
   projectId: string;
 
