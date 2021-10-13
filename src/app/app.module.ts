88c7af7dbeefaaef01e3a9c2bb39efa0b877b204
Timo <38291523+lovetodream@users.noreply.github.com>
2021-10-13 17:00:24 +0200

    ✨ Add project schema to mongoose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { AppService } from './app.service';
 import { AppResolver } from './app.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { App, AppSchema } from 'src/schemas/app.schema';
+import { Project, ProjectSchema } from 'src/schemas/project.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: App.name, schema: AppSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: App.name, schema: AppSchema },
+      { name: Project.name, schema: ProjectSchema },
+    ]),
+  ],
   providers: [AppService, AppResolver],
 })
 export class AppModule {}
