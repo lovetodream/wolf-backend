fed136e7bd5b2176e719438b47fa1f53dd448fc8
Timo <38291523+lovetodream@users.noreply.github.com>
2021-10-16 10:37:01 +0200

    🗃️ Add user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,27 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+@ObjectType()
+export class User {
+  @Field(() => String)
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Field()
+  @Prop()
+  email: string;
+
+  @Prop({ get: () => '' })
+  password: string;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
