ee765bbd2911155aac942ee5f37e0658ba69b7c2
Timo <38291523+lovetodream@users.noreply.github.com>
2021-10-19 21:57:55 +0200

    ✨ Add jwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'test-secret-dont-use-me-in-prod',
+    }),
   ],
   providers: [AuthService, AuthResolver],
 })
