d74de8ee985cdedbc9e9e882ba9e7db1413915a5
Timo <38291523+lovetodream@users.noreply.github.com>
2021-10-20 21:35:39 +0200

    🛂 Add jwt guard

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,23 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    super.logIn(request);
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
