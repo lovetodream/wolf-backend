ef40ee4fe8237f751f09c63f4977e9ea2bc0b621
Timo <38291523+lovetodream@users.noreply.github.com>
2021-11-01 12:39:48 +0100

    🐛 Fix dto for gql

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,12 +1,14 @@
-import { ArgsType } from '@nestjs/graphql';
+import { ArgsType, Field } from '@nestjs/graphql';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 @ArgsType()
 export class SignInDto {
   @IsEmail()
+  @Field()
   email: string;
 
   @IsString()
   @MinLength(8)
+  @Field()
   password: string;
 }
