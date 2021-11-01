8aac56f8ffb14834966b87ee5eceb26a190ed984
Timo <38291523+lovetodream@users.noreply.github.com>
2021-11-01 12:53:45 +0100

    ✨ Add different jwt extractors

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,12 +1,30 @@
 import { Injectable, SetMetadata } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { GqlContext } from './auth.resolver';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          console.log(req.cookies);
+          return (req.cookies['Authorization'] as string).replace(
+            'Bearer ',
+            '',
+          );
+        },
+        (ctx: Request) => {
+          return (
+            (ctx as unknown as GqlContext).req.cookies[
+              'Authorization'
+            ] as string
+          ).replace('Bearer ', '');
+        },
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET ?? 'test-secret-dont-use-me-in-prod',
     });
